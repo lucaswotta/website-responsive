a9d68572ce91c7e54f312938f652e4acc6acb8c7
lucaswotta <lucaswotta@gmail.com>
2022-10-25 00:19:52 -0300

    Add menu mobile

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let botao = document.querySelector('.menu-principal__btn');
+let menuPrincipal = document.querySelector('.menu-principal');
+botao.addEventListener("click", abreFechaMenu);
+
+function abreFechaMenu(evento) {
+    menuPrincipal.classList.toggle('menu-principal--fechado');
+}
